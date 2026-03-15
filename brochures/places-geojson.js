bb0961f700112a9dc7354f9aaacffa38ac1f3741
Richard Aroksaar <richard_aroksaar@nps.gov>
2026-03-15 06:04:14 -0700

    adding storage area for geojson

---
+++ brochures/places-geojson.js
@@ -0,0 +1,19 @@
+var parks = {
+		"type": "FeatureCollection",
+		"features": [
+				{
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+					29.249711, 
+					-103.161621
+					]
+				},
+				"type": "Feature",
+				"properties": {
+					"popupContent": 'Big Bend>'
+				},
+				"id": 52
+			},
+		]
+	};
