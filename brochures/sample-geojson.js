d0ee2934381c0d36765ee2a3332bcccc6388ccb5
Rick Aroksaar <raroksaar@gmail.com>
2026-03-16 14:33:20 -0700

    added catalog record link to Big Bend popup

--- brochures/sample-geojson.js
+++ brochures/sample-geojson.js
@@ -135,9 +135,7 @@ var park = {
             },
             "type": "Feature",
             "properties": {
-                "popupContent": '<a href="https://www.loc.gov/item/2011590439/">Big Bend visitor brochure on Library of Congress site </a> <br>'
-				// <a href="https://eosfcweb01.eosfc-intl.net/N94044/OPAC/Details/Record.aspx?BibCode=445224594">catalog record with link to HathiTrust copies </a>'
-				//https://eosfcweb01.eosfc-intl.net/N94044/OPAC/Details/Record.aspx?BibCode=445224594
+                "popupContent": '<a href="https://www.loc.gov/item/2011590439/">Big Bend visitor brochure on Library of Congress site </a> <br><a href="https://eosfcweb01.eosfc-intl.net/N94044/OPAC/Details/Record.aspx?BibCode=445224594">Catalog record with link to HathiTrust copies </a>'
             },
             "id": 57
         },
