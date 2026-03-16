288bedbfad6fc69955196a1bd8d5c6b0fbc11aae
Rick Aroksaar <raroksaar@gmail.com>
2026-03-16 11:17:00 -0700

    contains geojson cittions for park coordinates

---
+++ brochures/sample-geojson.js
@@ -0,0 +1,270 @@
+var freeBus = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "LineString",
+                "coordinates": [
+                    [-105.00341892242432, 39.75383843460583],
+                    [-105.0008225440979, 39.751891803969535]
+                ]
+            },
+            "properties": {
+                "popupContent": "This is a free bus line that will take you across downtown.",
+                "underConstruction": false
+            },
+            "id": 1
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "LineString",
+                "coordinates": [
+                    [-105.0008225440979, 39.751891803969535],
+                    [-104.99820470809937, 39.74979664004068]
+                ]
+            },
+            "properties": {
+                "popupContent": "This is a free bus line that will take you across downtown.",
+                "underConstruction": true
+            },
+            "id": 2
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "LineString",
+                "coordinates": [
+                    [-104.99820470809937, 39.74979664004068],
+                    [-104.98689651489258, 39.741052354709055]
+                ]
+            },
+            "properties": {
+                "popupContent": "This is a free bus line that will take you across downtown.",
+                "underConstruction": false
+            },
+            "id": 3
+        }
+    ]
+};
+
+var lightRailStop = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "popupContent": "18th & California Light Rail Stop"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-104.98999178409576, 39.74683938093904]
+            }
+        },{
+            "type": "Feature",
+            "properties": {
+                "popupContent": "20th & Welton Light Rail Stop"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-104.98689115047453, 39.747924136466565]
+            }
+        }
+    ]
+};
+
+var park = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+					 -88.322072, 35.151511
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Shiloh"},
+            "id": 51
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+					-77.22599, 39.81169, 
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Gettysburg"},
+            "id": 52
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+					-87.847979, 36.485720, 
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Fort Donelson"},
+            "id": 54
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9960754,
+                    39.7498956
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 55
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+				
+					-103.249574, 29.250897
+
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Big Bend"
+            },
+            "id": 57
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+				 -112.140447, 36.056233
+
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Grand Canyon"
+            },
+            "id": 58
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+				 -121.812736, 46.749694
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Mount Rainier"
+            },
+            "id": 74
+        },
+       {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+				 -112.140447, 36.056233
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Grand Canyon"
+            },
+            "id": 58
+        },
+        {
+           "geometry": {
+                "type": "Point",
+                "coordinates": [
+				 -119.588275, 37.749089
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Yosemite"
+            },
+            "id": 74
+        },
+
+    ]
+};
+
+
+var campus = {
+    "type": "Feature",
+    "properties": {
+        "popupContent": "This is the Auraria West Campus",
+        "style": {
+            weight: 2,
+            color: "#999",
+            opacity: 1,
+            fillColor: "#B0DE5C",
+            fillOpacity: 0.8
+        }
+    },
+    "geometry": {
+        "type": "MultiPolygon",
+        "coordinates": [
+            [
+                [
+                    [-105.00432014465332, 39.74732195489861],
+                    [-105.00715255737305, 39.74620006835170],
+                    [-105.00921249389647, 39.74468219277038],
+                    [-105.01067161560059, 39.74362625960105],
+                    [-105.01195907592773, 39.74290029616054],
+                    [-105.00989913940431, 39.74078835902781],
+                    [-105.00758171081543, 39.74059036160317],
+                    [-105.00346183776855, 39.74059036160317],
+                    [-105.00097274780272, 39.74059036160317],
+                    [-105.00062942504881, 39.74072235994946],
+                    [-105.00020027160645, 39.74191033368865],
+                    [-105.00071525573731, 39.74276830198601],
+                    [-105.00097274780272, 39.74369225589818],
+                    [-105.00097274780272, 39.74461619742136],
+                    [-105.00123023986816, 39.74534214278395],
+                    [-105.00183105468751, 39.74613407445653],
+                    [-105.00432014465332, 39.74732195489861]
+                ],[
+                    [-105.00361204147337, 39.74354376414072],
+                    [-105.00301122665405, 39.74278480127163],
+                    [-105.00221729278564, 39.74316428375108],
+                    [-105.00283956527711, 39.74390674342741],
+                    [-105.00361204147337, 39.74354376414072]
+                ]
+            ],[
+                [
+                    [-105.00942707061768, 39.73989736613708],
+                    [-105.00942707061768, 39.73910536278566],
+                    [-105.00685214996338, 39.73923736397631],
+                    [-105.00384807586671, 39.73910536278566],
+                    [-105.00174522399902, 39.73903936209552],
+                    [-105.00041484832764, 39.73910536278566],
+                    [-105.00041484832764, 39.73979836621592],
+                    [-105.00535011291504, 39.73986436617916],
+                    [-105.00942707061768, 39.73989736613708]
+                ]
+            ]
+        ]
+    }
+};
+
+var coorsField = {
+    "type": "Feature",
+    "properties": {
+        "popupContent": "Coors Field"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404191970824, 39.756213909328125]
+    }
+};
