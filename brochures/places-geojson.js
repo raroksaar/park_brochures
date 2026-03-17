8a6f2682d930d00641a48be93b096884aba91a13
Rick Aroksaar <raroksaar@gmail.com>
2026-03-17 09:33:00 -0700

    renamed sample ...

--- brochures/places-geojson.js
+++ brochures/places-geojson.js
@@ -1,19 +1,126 @@
-var parks = {
-		"type": "FeatureCollection",
-		"features": [
-				{
-				"geometry": {
-					"type": "Point",
-					"coordinates": [
-					29.249711, 
-					-103.161621
-					]
-				},
-				"type": "Feature",
-				"properties": {
-					"popupContent": 'Big Bend>'
-				},
-				"id": 52
-			},
-		]
-	};
+
+var park = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+					 -88.322072, 35.151511
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+				"popupContent": '<a href="https://crosssearch.nps.gov/brochures/parks/shiloh">Shiloh visitor brochure</a>'},
+            "id": 51
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+					-77.22599, 39.81169, 
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": '<a href="https://crosssearch.nps.gov/brochures/parks/gettysburg">Gettysburg visitor brochure</a>'},
+            "id": 52
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+					-87.847979, 36.485720 
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+				"popupContent": '<a href="https://crosssearch.nps.gov/brochures/parks/ftdonelson">Fort Donelson visitor brochure</a>'},
+            "id": 54
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [	
+					-105.560417, 40.366502
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Rocky Mountains"
+            },
+            "id": 55
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+					-103.249574, 29.250897
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": '<a href="https://www.loc.gov/item/2011590439/">Big Bend visitor brochure on Library of Congress site </a> <br><a href="https://eosfcweb01.eosfc-intl.net/N94044/OPAC/Details/Record.aspx?BibCode=445224594">Catalog record with link to HathiTrust copies </a>'
+            },
+            "id": 57
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+				 -112.140447, 36.056233
+
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Grand Canyon"
+            },
+            "id": 58
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+				 -121.812736, 46.749694
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": '<a href="https://www.nps.gov/mora/planyourvisit/upload/Mount-Rainier-Brochure-final_Combo_508_v2023.pdf">Mt Rainier visitor brochure</a><br>' 
+				// https://eosfcweb01.eosfc-intl.net/N94044/OPAC/Search/SimpleSearch.aspx?txtSearch=Mount+Rainier&selectUsing=2.0&rows=20
+            },
+            "id": 74
+        },
+       {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+				 -112.140447, 36.056233
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Grand Canyon"
+            },
+            "id": 58
+        },
+        {
+           "geometry": {
+                "type": "Point",
+                "coordinates": [
+				 -119.588275, 37.749089
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Yosemite"
+            },
+            "id": 74
+        },
+
+    ]
+};
+
+
+
