ec797aba8450336553ca28d2663a2400049d6062
Rick Aroksaar <raroksaar@gmail.com>
2026-04-23 13:06:26 -0700

    made file with link syntax

---
+++ park-brochures/links.js
@@ -0,0 +1,7 @@
+let LOWE_brochure_link = "<br><a href='https://www.loc.gov/item/80690757/'>Lowell visitor brochure</a><br>"
+let LOWE_catalog_link = "<a href='https://eosfcweb01.eosfc-intl.net/N94044/OPAC/Search/SimpleSearch.aspx?txtSearch=Lowell+Historical+Park+LOWE&selectUsing=2.0&rows=20>'>Library catalog search</a><br>"
+let LOWE_habs_4 = "<a href='https://www.loc.gov/item/ma0299/'>HABS-HAER - New Block boarding houses</a><br>"
+let LOWE_habs_3 = "<a href='https://www.loc.gov/item/ma0298/'>HABS-HAER - Town Hall</a><br>"
+let LOWE_habs_2 = "<a href='https://www.loc.gov/item/ma0297/'>HABS-HAER - Brick Block boarding houses</a><br>"
+let LOWE_habs_1 = "<a href='https://www.loc.gov/item/ma0296/'>HABS-HAER - Boott & Mass. Agents' House</a><br>"
+ 
