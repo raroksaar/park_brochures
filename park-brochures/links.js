e78f91b694644c4b513a6c3e45b3c0bb44f7b1e3
Rick Aroksaar <raroksaar@gmail.com>
2026-04-23 14:46:44 -0700

    adjusted Saratoga links

--- park-brochures/links.js
+++ park-brochures/links.js
@@ -5,5 +5,7 @@ let LOWE_habs_3 = "<a href='https://www.loc.gov/item/ma0298/'>HABS-HAER - Town H
 let LOWE_habs_2 = "<a href='https://www.loc.gov/item/ma0297/'>HABS-HAER - Brick Block boarding houses</a><br>"
 let LOWE_habs_1 = "<a href='https://www.loc.gov/item/ma0296/'>HABS-HAER - Boott & Mass. Agents' House</a><br>"
 
+let SARA_brochure_link ="<br><a href='http://npgallery.nps.gov/AssetDetail/3ac2bbec-90e0-4b02-ac83-6812ee1b4efb'>Saratoga Visitor brochure</a><br>"
+
 let SARA_catalog_link ="<a href='https://eosfcweb01.eosfc-intl.net/N94044/OPAC/Search/SimpleSearch.aspx?txtSearch=Saratoga+Historical+Park+SARA&selectUsing=2.0&rows=20>'>Library catalog search </a><br>"
  
