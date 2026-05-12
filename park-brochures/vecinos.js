a623e4b7d1ffaa153d8440978f3c18878d5b1d85
Rick Aroksaar <raroksaar@gmail.com>
2026-05-12 10:27:01 -0700

    added comments to show javascript arrays

---
+++ park-brochures/vecinos.js
@@ -0,0 +1,5 @@
+var vecinos= {"type":"FeatureCollection", "features": [
+  {"type":"Feature","geometry":{"type":"Point","coordinates":[-60.69440315176775,-31.637602967743867]},"properties":{"id":1,"nombre":"Moda Shop Boulevard","id_escuela":1,"direccion":"Blvd. Galvez 1799"}},
+  {"type":"Feature","geometry":{"type":"Point","coordinates":[-60.69419026517629,-31.63766801642459]},"properties":{"id":2,"nombre":"Banco Galicia","id_escuela":1,"direccion":"Blvd. Galvez 1769"}},
+  {"type":"Feature","geometry":{"type":"Point","coordinates":[-60.69483483846711,-31.638717665590832]},"properties":{"id":3,"nombre":"Verdulería Guadalupe","id_escuela":1,"direccion":"Necoechea 3373"}},
+]} 
