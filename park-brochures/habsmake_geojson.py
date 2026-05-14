94c93b06c36eed2a8af5ff8029e0154b5a08f173
Rick Aroksaar <raroksaar@gmail.com>
2026-05-14 15:39:34 -0700

    updated sample set with live LC links and python code to make geojson from tab delimited

--- park-brochures/habsmake_geojson.py
+++ park-brochures/habsmake_geojson.py
@@ -20,31 +20,33 @@ for line in f:
     if n > 100:
         break
     
-    if n > 10:
-        #print(fields)
-        #id	title	url	long_lat
-        id= fields[0]
-        title = fields[1]
-        state = 'all'
-        link = fields[2]
-        longlat = fields[3]   # -93.6767, 32.4981
-        lola = longlat.split(', ')
-        print(lola)
-        long = lola[0]
-        lat = lola[1]
-        point = (Point((float(long), float(lat))))
-        print(point)
-        
-        # Wrap it in a Feature with properties
-        feature = Feature(geometry=point, properties={"state": state, "popupContent": title+'<br>'+link})
-        #feature = Feature(geometry=point, properties="popupContent" +  title + '<br>' + link)
-        # Create a FeatureCollection
-        collection = FeatureCollection([feature])
-        #print(collection)
-        
-        print(str(collection))
-        o.write(str(collection))
-        o.write(',\n')
+    #print(fields)
+    #id	title	url	long_lat
+    id= fields[0]
+    title = fields[1]
+    state = title[-2:]
+    #print(state)
+
+    link = fields[2]
+    link = "<a href='" + link + "'>Library of Congress</a><br>"
+    #print(link)
+    longlat = fields[3]   # -93.6767, 32.4981
+    lola = longlat.split(', ')
+    #print(lola)
+    long = lola[0]
+    lat = lola[1]
+    point = (Point((float(long), float(lat))))
+    print(point)
+    
+    # Wrap it in a Feature with properties
+    feature = Feature(geometry=point, properties={"state": state, "popupContent": title+'<br>'+link})
+    # Create a FeatureCollection
+    collection = FeatureCollection([feature])
+    #print(collection)
+    
+    print(str(collection))
+    o.write(str(collection))
+    o.write(',\n')
 
 f.close()
 o.close()
